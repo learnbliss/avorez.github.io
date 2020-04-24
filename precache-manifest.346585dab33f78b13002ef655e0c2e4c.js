883494fbfa4471c78980e1c3714ad2b88af5f706
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2020-04-24 23:24:02 +0300

    Updates

---
+++ precache-manifest.346585dab33f78b13002ef655e0c2e4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0551afd3a938b1e5556fc25b2b2857d",
+    "url": "/avorez.github.io/index.html"
+  },
+  {
+    "revision": "b1eeb544dc2c2e076f3f",
+    "url": "/avorez.github.io/static/css/main.chunk.css"
+  },
+  {
+    "revision": "96af510e0622c79acf79",
+    "url": "/avorez.github.io/static/js/2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/avorez.github.io/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1eeb544dc2c2e076f3f",
+    "url": "/avorez.github.io/static/js/main.chunk.js"
+  },
+  {
+    "revision": "a1c53b726a1dc15ae558",
+    "url": "/avorez.github.io/static/js/runtime-main.3756ce45.js"
+  }
+]);
